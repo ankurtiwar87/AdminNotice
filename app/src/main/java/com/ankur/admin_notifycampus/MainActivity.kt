fb84b1137cd032a0c78d8d4f395a52a10e4be3f3
Ankur Tiwari <ankurtiwar87@gmail.com>
2023-09-11 17:37:31 +0530

    First Commit

---
+++ app/src/main/java/com/ankur/admin_notifycampus/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ankur.admin_notifycampus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
